d28f3a7775d314d1542dee903ce1ce4b5c36c875
Erivania Leal <erivanial.s1507@gmail.com>
2026-06-19 17:52:34 -0300

    feat: Método iniciarPausaLonga adicionado em UsuarioService

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioService.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioService.java
@@ -6,7 +6,10 @@ import dev.wakandaacademy.produdoro.usuario.application.api.UsuarioNovoRequest;
 import java.util.UUID;
 
 public interface UsuarioService {
-	UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo);
+    UsuarioCriadoResponse criaNovoUsuario(UsuarioNovoRequest usuarioNovo);
+
     UsuarioCriadoResponse buscaUsuarioPorId(UUID idUsuario);
+
+    void iniciarPausaLonga(UUID idUsuario, String usuario);
 
 }
