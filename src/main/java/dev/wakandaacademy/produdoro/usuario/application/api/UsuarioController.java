e38f159b4a691ab46591be9937331fcfbb223d09
Erivania Leal <erivanial.s1507@gmail.com>
2026-06-19 17:42:40 -0300

    feat: Endpoint iniciarPausaLonga no UsuarioController implementado

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioController.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioController.java
@@ -2,9 +2,16 @@ package dev.wakandaacademy.produdoro.usuario.application.api;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.wakandaacademy.produdoro.config.security.service.TokenService;
+import dev.wakandaacademy.produdoro.handler.APIException;
+import dev.wakandaacademy.produdoro.usuario.application.service.UsuarioApplicationService;
 import dev.wakandaacademy.produdoro.usuario.application.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -17,6 +24,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UsuarioController implements UsuarioAPI {
 	private final UsuarioService usuarioAppplicationService;
+	private final TokenService tokenService;
 
 	@Override
 	public UsuarioCriadoResponse postNovoUsuario(@Valid UsuarioNovoRequest usuarioNovo) {
@@ -25,6 +33,7 @@ public class UsuarioController implements UsuarioAPI {
 		log.info("[finaliza] UsuarioController - postNovoUsuario");
 		return usuarioCriado;
 	}
+
 	@Override
 	public UsuarioCriadoResponse buscaUsuarioPorId(UUID idUsuario) {
 		log.info("[inicia] UsuarioController - buscaUsuarioPorId");
@@ -33,4 +42,15 @@ public class UsuarioController implements UsuarioAPI {
 		log.info("[finaliza] UsuarioController - buscaUsuarioPorId");
 		return buscaUsuario;
 	}
+
+	@Override
+	public void iniciarPausaLonga(String token, UUID idUsuario) {
+		log.info("[inicia] UsuarioController - inciarPausaLonga");
+		String usuario = tokenService.getUsuarioByBearerToken(token)
+				.orElseThrow(() -> APIException.build(HttpStatus.FORBIDDEN, "Token inválido."));
+		usuarioAppplicationService.iniciarPausaLonga(idUsuario, usuario);
+		log.info("[finaliza] UsuarioController - iniciarPausaLonga");
+
+	}
+
 }
