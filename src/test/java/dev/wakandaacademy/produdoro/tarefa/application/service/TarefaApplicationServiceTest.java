6eb5e13047b1f170ba662c1924408a7d876d6021
PedroLucasCG <pedrowwww44@gmail.com>
2026-06-20 07:23:11 -0300

    feat: testes deservice do retorno quando o processo ocorre corretamente

--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -1,8 +1,6 @@
 package dev.wakandaacademy.produdoro.tarefa.application.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -10,6 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaResumidoResponse;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -31,6 +32,8 @@ class TarefaApplicationServiceTest {
     //	@MockBean
     @Mock
     TarefaRepository tarefaRepository;
+    @Mock
+    UsuarioRepository usuarioRepository;
 
     @Test
     void deveRetornarIdTarefaNovaCriada() {
@@ -45,13 +48,52 @@ class TarefaApplicationServiceTest {
     }
 
     @Test
-    void deveRetornarListaTarefaCriada() {
-        TarefaRequest request = getTarefaRequest();
-        when(tarefaRepository.salva(any())).thenReturn(new Tarefa(request));
+    void deveRetornarListaTarefasQuandoExecutadoComSucesso() {
+        List<Tarefa> tarefaResumidos = List.of(
+                new Tarefa(getTarefaRequest()),
+                new Tarefa(getTarefaRequest()),
+                new Tarefa(getTarefaRequest())
+        );
+
+        UUID uuid = UUID.randomUUID();
+        Usuario usuario = getUsuario(uuid);
+
+        when(tarefaRepository.buscaTarefasPorIdUsuario(any())).thenReturn(tarefaResumidos);
+        when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+
+        List<TarefaResumidoResponse> tarefaResumidosResponse = tarefaApplicationService.retornaTodasTarefas(uuid.toString(), uuid);
+
+        assertNotNull(tarefaResumidosResponse);
+        assertFalse(tarefaResumidosResponse.isEmpty());
+        assertEquals(TarefaResumidoResponse.class, tarefaResumidosResponse.get(0).getClass());
     }
+
+    @Test
+    void deveRetornarListaTarefasVaziaQuandoExecutadoComSucesso() {
+        List<Tarefa> tarefaResumidos = List.of();
+
+        UUID uuid = UUID.randomUUID();
+        Usuario usuario = getUsuario(uuid);
+
+        when(tarefaRepository.buscaTarefasPorIdUsuario(any())).thenReturn(tarefaResumidos);
+        when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+
+        List<TarefaResumidoResponse> tarefaResumidosResponse = tarefaApplicationService.retornaTodasTarefas(uuid.toString(), uuid);
+
+        assertNotNull(tarefaResumidosResponse);
+        assertTrue(tarefaResumidosResponse.isEmpty());
+    }
+
+
 
     public TarefaRequest getTarefaRequest() {
         TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
         return request;
+    }
+
+    public Usuario getUsuario(UUID uuid) {
+        return Usuario.builder().idUsuario(uuid).build();
     }
 }
