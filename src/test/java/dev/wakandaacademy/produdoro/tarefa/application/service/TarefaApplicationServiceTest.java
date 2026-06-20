331c6f7f9ae399d4d8b38e8ac133357b9472b269
PedroLucasCG <pedrowwww44@gmail.com>
2026-06-20 08:34:39 -0300

    feat: testes de exececao

--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -8,9 +8,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import dev.wakandaacademy.produdoro.handler.APIException;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaResumidoResponse;
 import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
 import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
+import feign.FeignException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -21,6 +24,7 @@ import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+import org.springframework.http.HttpStatus;
 
 @ExtendWith(MockitoExtension.class)
 class TarefaApplicationServiceTest {
@@ -62,7 +66,8 @@ class TarefaApplicationServiceTest {
         when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
         when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
 
-        List<TarefaResumidoResponse> tarefaResumidosResponse = tarefaApplicationService.retornaTodasTarefas(uuid.toString(), uuid);
+        List<TarefaResumidoResponse> tarefaResumidosResponse = tarefaApplicationService
+                .retornaTodasTarefas(uuid.toString(), uuid);
 
         assertNotNull(tarefaResumidosResponse);
         assertFalse(tarefaResumidosResponse.isEmpty());
@@ -80,16 +85,61 @@ class TarefaApplicationServiceTest {
         when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
         when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
 
-        List<TarefaResumidoResponse> tarefaResumidosResponse = tarefaApplicationService.retornaTodasTarefas(uuid.toString(), uuid);
+        List<TarefaResumidoResponse> tarefaResumidosResponse = tarefaApplicationService
+                .retornaTodasTarefas(uuid.toString(), uuid);
 
         assertNotNull(tarefaResumidosResponse);
         assertTrue(tarefaResumidosResponse.isEmpty());
     }
 
+    @Test
+    void deveRetornarExececao400BadRequestQuandoUsuarioIdNaoExistente() {
+        UUID uuid = UUID.randomUUID();
 
+        when(usuarioRepository.buscaUsuarioPorId(any()))
+                .thenThrow(APIException.build(HttpStatus.BAD_REQUEST, "Usuario não encontrado!"));
+
+        Exception exception = assertThrows(APIException.class, () -> tarefaApplicationService
+                .retornaTodasTarefas(uuid.toString(), uuid));
+
+        assertEquals("Usuario não encontrado!", exception.getMessage());
+        assertEquals(APIException.class, exception.getClass());
+    }
+
+    @Test
+    void deveRetornarExececao400BadRequestQuandoUsuarioEmailNaoExistente() {
+        UUID uuid = UUID.randomUUID();
+        Usuario usuario = getUsuario(uuid);
+
+        when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+        when(usuarioRepository.buscaUsuarioPorEmail(any()))
+                .thenThrow(APIException.build(HttpStatus.BAD_REQUEST, "Usuario não encontrado!"));
+
+        Exception exception = assertThrows(APIException.class, () -> tarefaApplicationService
+                .retornaTodasTarefas(uuid.toString(), uuid));
+
+        assertEquals("Usuario não encontrado!", exception.getMessage());
+        assertEquals(APIException.class, exception.getClass());
+    }
+
+    @Test
+    void deveRetornarExececao403ForbiddenQuandoUsuarioEmailNaoExistente() {
+        UUID uuid = UUID.randomUUID();
+        Usuario usuario = getUsuario(uuid);
+
+        when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+
+        Exception exception = assertThrows(APIException.class, () -> tarefaApplicationService
+                .retornaTodasTarefas(uuid.toString(), UUID.randomUUID()));
+
+        assertEquals("O usuário não têm acesso às tarefas.", exception.getMessage());
+        assertEquals(APIException.class, exception.getClass());
+    }
 
     public TarefaRequest getTarefaRequest() {
-        TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
+        TarefaRequest request =
+                new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
         return request;
     }
 
