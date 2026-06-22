a4e526421fb5e10a4f668c98a269709cfe715321
PedroLucasCG <pedrowwww44@gmail.com>
2026-06-22 20:37:18 -0300

    fix: imports desncessarios

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
@@ -1,11 +1,6 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
-import java.util.UUID;
-
-import javax.validation.Valid;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
