e152f24924ff30ecb21e343db1dbc3287ea793eb
Erivania Leal <erivanial.s1507@gmail.com>
2026-06-26 19:51:27 -0300

    feat(controller): encaminha usuário autenticado e id para o service

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
@@ -30,7 +30,7 @@ public class TarefaRestController implements TarefaAPI {
 	public TarefaDetalhadoResponse detalhaTarefa(String token, UUID idTarefa) {
 		log.info("[inicia] TarefaRestController - detalhaTarefa");
 		String usuario = getUsuarioByToken(token);
-		Tarefa tarefa = tarefaService.detalhaTarefa(usuario,idTarefa);
+		Tarefa tarefa = tarefaService.detalhaTarefa(usuario, idTarefa);
 		log.info("[finaliza] TarefaRestController - detalhaTarefa");
 		return new TarefaDetalhadoResponse(tarefa);
 	}
@@ -45,9 +45,19 @@ public class TarefaRestController implements TarefaAPI {
 
 	private String getUsuarioByToken(String token) {
 		log.debug("[token] {}", token);
-		String usuario = tokenService.getUsuarioByBearerToken(token).orElseThrow(() -> APIException.build(HttpStatus.UNAUTHORIZED, token));
+		String usuario = tokenService.getUsuarioByBearerToken(token)
+				.orElseThrow(() -> APIException.build(HttpStatus.UNAUTHORIZED, token));
 		log.info("[usuario] {}", usuario);
 		return usuario;
+	}
+
+	@Override
+	public void limparTodasTarefas(String token, UUID idUsuario) {
+		log.info("[iniciar] TarefaRestController - limparTodasTarefas");
+		String usuario = getUsuarioByToken(token);
+		tarefaService.limparTodasTarefas(usuario, idUsuario);
+		log.info("[finaliza] TarefaRestController - limparTodasTarefas");
+
 	}
 
 }
