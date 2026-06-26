73167602928331b5d8038dc33b9b713b025f3149
Erivania Leal <erivanial.s1507@gmail.com>
2026-06-26 19:43:00 -0300

    feat: adiciona endpoint para limpar todas as tarefas

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
@@ -9,19 +9,23 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/v1/tarefa")
 public interface TarefaAPI {
-    @PostMapping
-    @ResponseStatus(code = HttpStatus.CREATED)
-    TarefaIdResponse postNovaTarefa(@RequestBody @Valid TarefaRequest tarefaRequest);
+        @PostMapping
+        @ResponseStatus(code = HttpStatus.CREATED)
+        TarefaIdResponse postNovaTarefa(@RequestBody @Valid TarefaRequest tarefaRequest);
 
-    @GetMapping("/{idTarefa}")
-    @ResponseStatus(code = HttpStatus.OK)
-    TarefaDetalhadoResponse detalhaTarefa(@RequestHeader(name = "Authorization", required = true) String token,
-                                          @PathVariable UUID idTarefa);
+        @GetMapping("/{idTarefa}")
+        @ResponseStatus(code = HttpStatus.OK)
+        TarefaDetalhadoResponse detalhaTarefa(@RequestHeader(name = "Authorization", required = true) String token,
+                        @PathVariable UUID idTarefa);
 
-    @PatchMapping("/{idTarefa}/concluir")
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    void concluiTarefa(
-            @RequestHeader(name = "Authorization", required = true) String token,
-            @PathVariable UUID idTarefa
-    );
+        @PatchMapping("/{idTarefa}/concluir")
+        @ResponseStatus(code = HttpStatus.NO_CONTENT)
+        void concluiTarefa(
+                        @RequestHeader(name = "Authorization", required = true) String token,
+                        @PathVariable UUID idTarefa);
+
+        @DeleteMapping("/{idUsuario}/limpar-todas-tarefas")
+        @ResponseStatus(code = HttpStatus.NO_CONTENT)
+        void limparTodasTarefas(@RequestHeader(name = "Authorization", required = true) String token,
+                        @PathVariable UUID idUsuario);
 }
