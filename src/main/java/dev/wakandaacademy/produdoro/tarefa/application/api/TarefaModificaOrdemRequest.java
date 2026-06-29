ceba8addacc3cab43c6ab5ede916f25409ff81c6
Marcelo Brito <mhnbrito6@gmail.com>
2026-06-29 20:30:28 -0300

    bug fix: Corrigido bug no DTO

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaModificaOrdemRequest.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaModificaOrdemRequest.java
@@ -1,12 +1,19 @@
 package dev.wakandaacademy.produdoro.tarefa.application.api;
 
-import lombok.Value;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
 import javax.validation.constraints.PositiveOrZero;
 
-@Value
+@Getter
 public class TarefaModificaOrdemRequest {
 
     @PositiveOrZero
-    int novaPosicao;
+    private final int novaPosicao;
+
+    @JsonCreator
+    public TarefaModificaOrdemRequest(@JsonProperty("novaPosicao") int novaPosicao) {
+        this.novaPosicao = novaPosicao;
+    }
 }
